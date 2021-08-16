96561a4b11fbc31dac0e1e8d5a43addc1e55d068
Yrian Derreumaux <50255125+yrianderreumaux@users.noreply.github.com>
2021-08-16 11:00:10 -0700

    Add files via upload

---
+++ rel-high.js
@@ -0,0 +1,523 @@
+
+/**
+
+ * @param context {WebtaskContext}
+
+ */
+
+module.exports
+    =
+    function(context,
+             cb)
+    {
+
+
+
+        var
+            jsonContent
+                =
+                {
+
+                    "sampling":
+                        [
+
+                            {"GHP-J data":
+                                    116
+                            },
+
+                            {"GHP-J data":
+                                    92
+                            },
+
+                            {"GHP-J data":
+                                    97
+                            },
+
+                            {"GHP-J data":
+                                    92
+                            },
+
+                            {"GHP-J data":
+                                    94
+                            },
+
+                            {"GHP-J data":
+                                    108
+                            },
+
+                            {"GHP-J data":
+                                    100
+                            },
+
+                            {"GHP-J data":
+                                    96
+                            },
+
+                            {"GHP-J data":
+                                    114
+                            },
+
+                            {"GHP-J data":
+                                    80
+                            },
+
+                            {"GHP-J data":
+                                    114
+                            },
+
+                            {"GHP-J data":
+                                    95
+                            },
+
+                            {"GHP-J data":
+                                    104
+                            },
+
+                            {"GHP-J data":
+                                    127
+                            },
+
+                            {"GHP-J data":
+                                    101
+                            },
+
+                            {"GHP-J data":
+                                    91
+                            },
+
+                            {"GHP-J data":
+                                    90
+                            },
+
+                            {"GHP-J data":
+                                    111
+                            },
+
+                            {"GHP-J data":
+                                    105
+                            },
+
+                            {"GHP-J data":
+                                    98
+                            },
+
+                            {"GHP-J data":
+                                    100
+                            },
+
+                            {"GHP-J data":
+                                    112
+                            },
+
+                            {"GHP-J data":
+                                    110
+                            },
+
+                            {"GHP-J data":
+                                    80
+                            },
+
+                            {"GHP-J data":
+                                    108
+                            },
+
+                            {"GHP-J data":
+                                    108
+                            },
+
+                            {"GHP-J data":
+                                    91
+                            },
+
+                            {"GHP-J data":
+                                    99
+                            },
+
+                            {"GHP-J data":
+                                    114
+                            },
+
+                            {"GHP-J data":
+                                    115
+                            },
+
+                            {"GHP-J data":
+                                    95
+                            },
+
+                            {"GHP-J data":
+                                    86
+                            },
+
+                            {"GHP-J data":
+                                    99
+                            },
+
+                            {"GHP-J data":
+                                    118
+                            },
+
+                            {"GHP-J data":
+                                    100
+                            },
+
+                            {"GHP-J data":
+                                    102
+                            },
+
+                            {"GHP-J data":
+                                    97
+                            },
+
+                            {"GHP-J data":
+                                    87
+                            },
+
+                            {"GHP-J data":
+                                    109
+                            },
+
+                            {"GHP-J data":
+                                    114
+                            },
+
+                            {"GHP-J data":
+                                    111
+                            },
+
+                            {"GHP-J data":
+                                    102
+                            },
+
+                            {"GHP-J data":
+                                    116
+                            },
+
+                            {"GHP-J data":
+                                    90
+                            },
+
+                            {"GHP-J data":
+                                    101
+                            },
+
+                            {"GHP-J data":
+                                    108
+                            },
+
+                            {"GHP-J data":
+                                    88
+                            },
+
+                            {"GHP-J data":
+                                    98
+                            },
+
+                            {"GHP-J data":
+                                    93
+                            },
+
+                            {"GHP-J data":
+                                    106
+                            },
+
+                            {"GHP-J data":
+                                    100
+                            },
+
+                            {"GHP-J data":
+                                    105
+                            },
+
+                            {"GHP-J data":
+                                    97
+                            },
+
+                            {"GHP-J data":
+                                    106
+                            },
+
+                            {"GHP-J data":
+                                    90
+                            },
+
+                            {"GHP-J data":
+                                    116
+                            },
+
+                            {"GHP-J data":
+                                    94
+                            },
+
+                            {"GHP-J data":
+                                    92
+                            },
+
+                            {"GHP-J data":
+                                    112
+                            },
+
+                            {"GHP-J data":
+                                    107
+                            },
+
+                            {"GHP-J data":
+                                    109
+                            },
+
+                            {"GHP-J data":
+                                    104
+                            },
+
+                            {"GHP-J data":
+                                    114
+                            },
+
+                            {"GHP-J data":
+                                    93
+                            },
+
+                            {"GHP-J data":
+                                    113
+                            },
+
+                            {"GHP-J data":
+                                    111
+                            },
+
+                            {"GHP-J data":
+                                    100
+                            },
+
+                            {"GHP-J data":
+                                    106
+                            },
+
+                            {"GHP-J data":
+                                    99
+                            },
+
+                            {"GHP-J data":
+                                    105
+                            },
+
+                            {"GHP-J data":
+                                    101
+                            },
+
+                            {"GHP-J data":
+                                    82
+                            },
+
+                            {"GHP-J data":
+                                    91
+                            },
+
+                            {"GHP-J data":
+                                    105
+                            },
+
+                            {"GHP-J data":
+                                    100
+                            },
+
+                            {"GHP-J data":
+                                    95
+                            },
+
+                            {"GHP-J data":
+                                    102
+                            },
+
+                            {"GHP-J data":
+                                    101
+                            },
+
+                            {"GHP-J data":
+                                    97
+                            },
+
+                            {"GHP-J data":
+                                    109
+                            },
+
+                            {"GHP-J data":
+                                    114
+                            },
+
+                            {"GHP-J data":
+                                    114
+                            },
+
+                            {"GHP-J data":
+                                    123
+                            },
+
+                            {"GHP-J data":
+                                    97
+                            },
+
+                            {"GHP-J data":
+                                    105
+                            },
+
+                            {"GHP-J data":
+                                    92
+                            },
+
+                            {"GHP-J data":
+                                    102
+                            },
+
+                            {"GHP-J data":
+                                    84
+                            },
+
+                            {"GHP-J data":
+                                    95
+                            },
+
+                            {"GHP-J data":
+                                    96
+                            },
+
+                            {"GHP-J data":
+                                    100
+                            },
+
+                            {"GHP-J data":
+                                    111
+                            },
+
+                            {"GHP-J data":
+                                    102
+                            },
+
+                            {"GHP-J data":
+                                    89
+                            },
+
+                            {"GHP-J data":
+                                    117
+                            },
+
+                            {"GHP-J data":
+                                    106
+                            },
+
+                            {"GHP-J data":
+                                    103
+                            },
+
+                            {"GHP-J data":
+                                    120
+                            },
+
+                            {"GHP-J data":
+                                    103
+                            },
+
+                            {"GHP-J data":
+                                    113
+                            },
+
+                            {"GHP-J data":
+                                    107
+                            },
+
+                            {"GHP-J data":
+                                    107
+                            },
+
+                            {"GHP-J data":
+                                    109
+                            },
+
+                            {"GHP-J data":
+                                    110
+                            },
+
+                            {"GHP-J data":
+                                    83
+                            },
+
+                            {"GHP-J data":
+                                    100
+                            },
+
+                            {"GHP-J data":
+                                    122
+                            },
+
+                            {"GHP-J data":
+                                    104
+                            },
+
+                            {"GHP-J data":
+                                    110
+                            },
+
+                            {"GHP-J data":
+                                    108
+                            },
+
+                            {"GHP-J data":
+                                    101
+                            },
+
+                            {"GHP-J data":
+                                    98
+                            },
+
+                            {"GHP-J data":
+                                    105
+                            },
+
+                            {"GHP-J data":
+                                    86
+                            },
+
+                            {"GHP-J data":
+                                    94
+                            },
+
+                            {"GHP-J data":
+                                    90
+                            },
+
+                            {"GHP-J data":
+                                    120
+                            },
+
+                            {"GHP-J data":
+                                    81
+                            },
+
+                            {"GHP-J data":
+                                    115
+                            },
+
+                            {"GHP-J data":
+                                    99
+                            }
+
+
+
+                        ]
+
+                };
+
+
+
+        var
+            random
+                = jsonContent.sampling[Math.floor(Math.random()
+                *
+                jsonContent.sampling.length)];
+
+
+        cb(null,
+            random);
+
+    };
+
