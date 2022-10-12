f5737615095fb08309004ea50cec0e6f95cc2720
Yrian Derreumaux <yderr001@ucr.edu>
2022-10-11 17:01:19 -0700

    add new dist

--- Distribution_Script.R
+++ Distribution_Script.R
@@ -3,9 +3,9 @@ library(truncnorm)
 library(psych)
 
 set.seed(333)
-C1A <- as.integer(rtruncnorm(100, a=0, b=100, mean = 68, sd = 6))
-C1B <- as.integer(rtruncnorm(100, a=0, b=100, mean = 68, sd = 12))
-C2A <- as.integer(rtruncnorm(100, a=0, b=100, mean = 68, sd = 16))
+C1A <- as.integer(rtruncnorm(200, a=0, b=100, mean = 68, sd = 6))
+C1B <- as.integer(rtruncnorm(200, a=0, b=100, mean = 68, sd = 12))
+C2A <- as.integer(rtruncnorm(200, a=0, b=100, mean = 68, sd = 18))
 
 #hist
 hist(C1A)
@@ -17,6 +17,6 @@ describe(C1A)
 describe(C1B)
 describe(C2A)
 
-write.csv(C1A, "C1A_(M=67.30,SD=5.75).csv")
+write.csv(C1A, "/Users/yrianderreumaux/Documents/GitHub/reliability/C1A_(M=67.30,SD=5.75)_2022.csv")
 write.csv(C1B, "C1B_(M=68.06,SD=11.2).csv")
-write.csv(C2A, "C2A_(M=67.05,SD=16.66).csv")
+write.csv(C2A, "/Users/yrianderreumaux/Documents/GitHub/reliability/C2A_(M=67.05,SD=16.66)_2022.csv")
